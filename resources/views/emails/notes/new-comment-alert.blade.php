1c185ae6c8c5b638b1fe01f61cd416dfe34a21a9
skid06 <valencia.mel.06@gmail.com>
2019-06-29 20:49:11 +0800

    Changed the url in the mailable template.

--- resources/views/emails/notes/new-comment-alert.blade.php
+++ resources/views/emails/notes/new-comment-alert.blade.php
@@ -4,13 +4,13 @@
 @if($note->user_id == null)
   An admin added a note for you
 
-  @component('mail::button', ['url' => env('APP_URL').'/liquor-application/'.$note->application_id])
+  @component('mail::button', ['url' => 'http://159.203.109.4/liquor-application/'.$note->application_id])
     View the application's notes
   @endcomponent
 @elseif($note->admin_id == null)
   A user added a note for you.
 
-  @component('mail::button', ['url' => url().'/admin/liquor-application/'.$note->application_id])
+  @component('mail::button', ['url' => 'http://159.203.109.4/admin/liquor-application/'.$note->application_id])
     View the application's notes
   @endcomponent  
 @endif
