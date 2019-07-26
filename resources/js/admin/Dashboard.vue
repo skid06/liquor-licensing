8f9b46838f40634fc3c71432304cc56c89dd1ad9
skid06 <valencia.mel.06@gmail.com>
2019-07-27 00:40:52 +0800

    Getting  the last notes on the applications.

--- resources/js/admin/Dashboard.vue
+++ resources/js/admin/Dashboard.vue
@@ -85,10 +85,10 @@
           <template v-slot:items="props">
             <td>{{ props.item.business_name }}</td>
             <td class="text-xs-left">{{ props.item.business_address }}</td>
+            <td class="text-xs-left">{{ props.item.business_email }}</td>
             <td class="text-xs-left">{{ props.item.business_contact_person }}</td>
             <td class="text-xs-left">{{ props.item.business_classification }}</td>
             <td class="text-xs-left">{{ props.item.updated_at | moment("MMM DD, YYYY")  }}</td>
-            <td class="text-xs-left">{{ props.item.notes[props.item.notes.length - 1].message }}</td>
             <td class="justify-center layout px-0">
               <v-icon
                 small
