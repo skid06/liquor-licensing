11af80b9af78889cccf5f9de40979f261f35c67a
skid06 <valencia.mel.06@gmail.com>
2019-07-26 22:39:22 +0800

    Added new pdf layout.

--- resources/views/liquor-application-pdf.blade.php
+++ resources/views/liquor-application-pdf.blade.php
@@ -84,37 +84,37 @@ textarea{
 </head>
 <body>
     <div class="container-fluid">
-		<div class="container" style="max-width:50% !important">
+		<div class="container" style="max-width:50% !important; font-family:monospace;">
       <div style="width: 50%; margin: 0 auto;"> <h3>ORDINANCE NO.</h3> </div>
-      <div style="text-align: justify;">
+      <p style="text-align: center !important;" style="text-align: center !important;">
               AN ORDINANCE APPROVING ISSUANCE OF A CLASS "F" LIQUOR LICENSE FOR
-        NINO'S INVESTMENTS INC. d/b/a "Lucky Slots 2" PURSUANT TO CHAPTER 6,
+              {{ @$pdf['business_name'] }} PURSUANT TO CHAPTER 6,
         SECTION 6-37 OF THE CODE OF ORDINANCES OF THE CITY OF LOVES PARK,
         ILLINOIS
-      </div>
-      <p>
+      </p>
+      <p style="text-align: center !important;">
       WHEREAS, Chapter 6, Sections 6-37 and 6-43 of the Code of Ordinances of the City of
 Loves Park ("City") regulate the number and classes of liquor licenses, respectively, existing
 within the City ("Code"); and
       </p>
-      <p>
+      <p style="text-align: center !important;">
       WHEREAS, a Class "F" liquor license authorizes the retail sale and consumption of
 alcoholic liquor within a licensed premises, provided that the licensed premises also has, locates,
 maintains and lawfully operates Video Gaming Terminals (as defined under the Illinois Video
 Gaming Act, 230 ILCS 40/1 et seq.) within the Class "F" licensed establishment; and
       </p>
-      <p>
+      <p style="text-align: center !important;">
       WHEREAS, the City now desires to approve the issuance of a Class "F" liquor license to
 Nino's Investments Inc., an Illinois corporation, doing business as "Lucky Slots 2" (hereinafter
 "Licensee"), pursuant to Chapter 6, Section 6-37 and 6-43 of the Code; and
       </p>
-      <p>
+      <p style="text-align: center !important;">
       WHEREAS, said liquor license shall be for use only within the portion of the premises
 located at 6451 North Second Street, Loves Park, Illinois, 61111 which is exclusively used for
 video gaming and occupied by Licensee (“Premises”) and subject to the conditions set forth
 herein.
       </p>
-      <p>
+      <p style="text-align: center !important;">
       NOW THEREFORE, BE IT ORDAINED BY THE CITY COUNCIL OF THE CITY OF
 LOVES PARK, BOONE AND WINNEBAGO COUNTIES, ILLINOIS AS FOLLOWS:
       </p>
@@ -153,14 +153,14 @@ thereof in conflict with the provisions of this Ordinance are hereby repealed.
 and publication in pamphlet form as provided by law.
         </li>
       </ol>
-      <p>
+      <p style="text-align: center !important;">
       PASSED by the City Council of the City of Loves Park this ____day of May, 2018.
       </p>
-      <p>
+      <p style="text-align: center !important;">
         APPROVED: <br></br>
         Mayor: 
       </p>
-      <p>
+      <p style="text-align: center !important;">
         ATTEST: <br>
         City Clerk: 
 
