82bab7331ea7a284871a912cbd957f1864c7f69b
skid06 <valencia.mel.06@gmail.com>
2019-07-27 01:16:35 +0800

    Modified the email template for processed application event.

--- resources/views/emails/application/processedapplicationalert.blade.php
+++ resources/views/emails/application/processedapplicationalert.blade.php
@@ -1,9 +1,6 @@
 @component('mail::message')
 # Congratulation! Your application has been processed.
 
-@component('mail::panel')
-There is a new Application ready to be processed now.  
-@endcomponent
 
 @component('mail::button', ['url' => config('app.url').'/applications/processed'])
 View Processed Applications
